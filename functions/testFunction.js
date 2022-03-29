75cfd2e3d2e6e70f7d2ea91fd7c2b1712772463e
torontocaper <torontocaper@gmail.com>
2022-03-29 10:19:40 -0400

    attempting to move everything to parent folder

---
+++ functions/testFunction.js
@@ -0,0 +1,18 @@
+
+// This is your new function. To start, set the name and path on the left.
+
+exports.handler = function(context, event, callback) {
+    // Here's an example of setting up some TWiML to respond to with this function
+      let twiml = new Twilio.twiml.VoiceResponse();
+    twiml.say('Hello World');
+      
+    let variable = 'welcome!';
+  
+    // You can log with console.log
+    console.log('error', variable);
+  
+    // This callback is what is returned in response to this function being invoked.
+    // It's really important! E.g. you might respond with TWiML here for a voice or SMS response.
+    // Or you might return JSON data to a studio flow. Don't forget it!
+    return callback(null, twiml);
+  };
