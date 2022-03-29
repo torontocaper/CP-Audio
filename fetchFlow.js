7d1ecda596a0a053103238c70941f048da5869b1
torontocaper <torontocaper@gmail.com>
2022-03-29 11:57:32 -0400

    got fetchFlow working

--- fetchFlow.js
+++ fetchFlow.js
@@ -1,7 +1,10 @@
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
+const dotenv = require("dotenv").config({ path: '/_Code/CP Audio/.env' })
+const accountSid = process.env.ACCOUNT_SID;
+const authToken = process.env.AUTH_TOKEN;
+
 const client = require('twilio')(accountSid, authToken);
 
 client.studio.flows('FW59ba18decc7add221ede94d0f24e6974')
              .fetch()
              .then(flow => console.log(flow.friendlyName));
+ 
