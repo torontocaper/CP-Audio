2e321b3877dcb3bb660db0552d788333066eb95a
torontocaper <torontocaper@gmail.com>
2022-03-29 14:11:46 -0400

    added file and function for latest flow revision

--- fetchPublished.js
+++ fetchPublished.js
@@ -6,6 +6,12 @@ const fs = require('fs');
 const client = require('twilio')(accountSid, authToken);
 
 client.studio.flows('FW59ba18decc7add221ede94d0f24e6974')
-             .revisions("LatestPublished")
-             .fetch()
-             .then(flow => fs.writeFileSync("CP Audio/flowPublished.json", (JSON.stringify(flow, null, 2))))
+    .revisions("LatestPublished")
+    .fetch()
+    .then(flow => fs.writeFileSync("CP Audio/flowPublished.json", (JSON.stringify(flow, null, 2))))
+
+client.studio.flows('FW59ba18decc7add221ede94d0f24e6974')
+    .revisions("LatestRevision")
+    .fetch()
+    .then(flow => fs.writeFileSync("CP Audio/flowLatestRevision.json", (JSON.stringify(flow, null, 2))))
+
